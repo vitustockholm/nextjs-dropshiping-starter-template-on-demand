3e9211469615c000b8a85fcd783902be380f2541
vitustockholm <vitustockholm@gmail.com>
2021-12-02 14:04:26 +0200

    testing redeploy

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 
 module.exports = {
   images: {
-    domains: [
-      "assets.vercel.com",
-      "nextjs-dropshiping-starter-template-on-demand-vitustockholm.vercel.app",
-      "localhost:3000",
-      "files.cdn.printful.com",
-    ],
+    domains: ["files.cdn.printful.com"],
   },
 };
