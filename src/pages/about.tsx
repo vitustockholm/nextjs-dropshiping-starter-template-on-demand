c3bd8e14e3d3a58571fcd12968ec58eabc30811e
vitustockholm <vitustockholm@gmail.com>
2021-12-03 23:01:06 +0200

    changed data info in about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,18 +2,7 @@ function TermsPage() {
   return (
     <div className="max-w-xl mx-auto prose prose-blue">
       <h2>Welcome</h2>
-      <p>
-        You're viewing the{" "}
-        <a
-          href="https://headlessdropshipping.com/"
-          title="Learn more about Headless Dropshipping"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Headless Dropshipping Starter
-        </a>{" "}
-        by Vytautas B.
-      </p>
+      Headless Dropshipping Starter by Vytautas B.
       <p>
         Build with Next.js and Snipcart. End to end commerce, styled using
         TailwindCSS and deployed on Vercel.
